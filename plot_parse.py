6b026d67fd0cff0d7821525d1406ba4c5ffae366
James <james.kizer@gmail.com>
2014-12-03 01:33:05 -0500

    Finished computation for 2

--- plot_parse.py
+++ plot_parse.py
@@ -6,8 +6,12 @@ import random
 import string
 from math import log as log
 
-#def 
+from datetime import datetime
+import numpy as np
+import stop_words
 
+#def 
+stopWords = {str(word):0 for word in stop_words.get_stop_words('English')}
 
 moviesDict = {}
 def load_all_movies(filename):
@@ -75,6 +79,8 @@ def lineReducer(line):
     lineDict = {}
     for word in line.split():
         word = word.translate(None, string.punctuation).lower()
+        if word in stopWords:
+            continue
         if word in lineDict:
             lineDict[word] += 1
         else:
@@ -104,32 +110,34 @@ def generateProbabilityFunctionForData(dataset):
     wordDictionary = reduce(dictReducer, [movie['word_map'] for movie in dataset], {})
     datasetLength = len(dataset)
     #generate the bag of words for this decade
-    bagOfWords = {key:{} for key in wordDictionary.iterkeys()}
+    bagOfWords = {key:{0: len(dataset)} for key in wordDictionary.iterkeys()}
 
     #for each movie in the dataset, vote for the number of times the word is used in the summary
     for movie in dataset:
-        for word in bagOfWords.iterkeys():
+        for word in movie['word_map']:
+            #print word
+            value = movie['word_map'][word]
 
-            #determine how many times the word is used
-            if word in movie['word_map']:
-                value = movie['word_map'][word]
-            else:
-                value = 0
-
-            #vote in the decade-wide bag of words
             if value in bagOfWords[word]:
                 bagOfWords[word][value] += 1
             else:
                 bagOfWords[word][value] = 1
 
-    #print bagOfWords['the']
-    #function returns #votes for #times the word shows up in the plot summary
-    #divided by the total number of entries in the dataset
+            bagOfWords[word][0] -= 1
 
-    #note that if the word didnt exist in the original bag of words we 
-    #return very high probability for xi=0, otherwise very small probability
+        # for word in bagOfWords.iterkeys():
 
+        #     #determine how many times the word is used
+        #     if word in movie['word_map']:
+        #         value = movie['word_map'][word]
+        #     else:
+        #         value = 0
 
+        #     #vote in the decade-wide bag of words
+        #     if value in bagOfWords[word]:
+        #         bagOfWords[word][value] += 1
+        #     else:
+        #         bagOfWords[word][value] = 1
 
     def probabilityFunction(wi, xi):
         if wi in bagOfWords:
@@ -149,6 +157,63 @@ def generateProbabilityFunctionForData(dataset):
                 return dirlect
 
     return probabilityFunction
+
+# def generateProbabilityFunctionForDataOld(dataset):
+
+
+#     #get bag of words for entire data set
+#     #initialize bag of words
+#     # each word has a dictionary containing the number of times the word is present and the number of movies voting for it
+#     wordDictionary = reduce(dictReducer, [movie['word_map'] for movie in dataset], {})
+#     datasetLength = len(dataset)
+#     #generate the bag of words for this decade
+#     bagOfWords = {key:{} for key in wordDictionary.iterkeys()}
+
+#     #for each movie in the dataset, vote for the number of times the word is used in the summary
+#     for movie in dataset:
+
+#         for word in bagOfWords.iterkeys():
+
+#             #determine how many times the word is used
+#             if word in movie['word_map']:
+#                 value = movie['word_map'][word]
+#             else:
+#                 value = 0
+
+#             #vote in the decade-wide bag of words
+#             if value in bagOfWords[word]:
+#                 bagOfWords[word][value] += 1
+#             else:
+#                 bagOfWords[word][value] = 1
+
+
+#     #print bagOfWords['the']
+#     #function returns #votes for #times the word shows up in the plot summary
+#     #divided by the total number of entries in the dataset
+
+#     #note that if the word didnt exist in the original bag of words we 
+#     #return very high probability for xi=0, otherwise very small probability
+
+
+
+#     def probabilityFunction(wi, xi):
+#         if wi in bagOfWords:
+#             if xi in bagOfWords[wi]:
+#                 if bagOfWords[wi][xi] == 0:
+#                     return dirlect
+#                 else:
+#                     probability = float(bagOfWords[wi][xi])/float(datasetLength)
+#                     #print 'computed probability: {}'.format(probability)
+#                     return probability
+#             else:
+#                 return dirlect
+#         else:
+#             if xi == 0:
+#                 return maxProb
+#             else:
+#                 return dirlect
+
+#     return probabilityFunction
 
 
 
@@ -218,7 +283,10 @@ def generateProbabilityFunctionForData(dataset):
 #         if 
 
 if __name__ == '__main__':
+
+    print 'Start time: {}'.format(datetime.now())
     all_movies = list(load_all_movies("./plot.list.gz"))
+    print 'Loaded all movies time: {}'.format(datetime.now())
     # all_movies.append({"title": "dummy",
     #                              "year": 2020,
     #                              'identifier': 'identifier',
@@ -227,7 +295,7 @@ if __name__ == '__main__':
 
     #print('min year = {}, max year = {}'.format(min_year, max_year))
 
-    first = load_all_movies("./plot.list.gz").next()
+    #first = load_all_movies("./plot.list.gz").next()
     #print first['summary']
 
     # lineDictList = []
@@ -273,30 +341,44 @@ if __name__ == '__main__':
 
     def generateTrainingAndTestData(movieData):
         decades = [1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010]
-        #balanced_movies = []
+        balanced_movies = []
         trainingSet = []
         testSet = []
+
+        train_x = []
+        test_x = []
+        train_y = []
+        test_y = []
+
 
         for decade in decades:
             decade_data = [movie for movie in movieData if movie['year']==decade]
             if(len(decade_data) < 6000):
                 print 'Decade {} contains fewer than 6000 movies'.format(decade)
-            decade_subset = random.sample(decade_data, 60)
+            decade_subset = random.sample(decade_data, 6000)
             balanced_movies.extend(decade_subset)
-            balanced_movies_train = random.sample(decade_subset,48)
+            balanced_movies_train = random.sample(decade_subset,4800)
+            trainingSet.extend(balanced_movies_train)
             balanced_movies_test = [movie for movie in decade_subset if movie not in balanced_movies_train]
+            testSet.extend(balanced_movies_test)
             train_x.append( [movie['summary'] for movie in balanced_movies_train])
             test_x.append( [movie['summary'] for movie in balanced_movies_test])
             train_y.append( [movie['year'] for movie in balanced_movies_train])
             test_y.append( [movie['year'] for movie in balanced_movies_test])
+
+        #return (balanced_movies,)
 
         return (balanced_movies, trainingSet, testSet, train_x, test_x, train_y, test_y)
 
 
     ret = generateTrainingAndTestData(all_movies)
     balanced_movies = ret[0]
+    #trainingSet = balanced_movies
     trainingSet = ret[1]
+    print 'Training Set Len: {}'.format(len(trainingSet))
     testSet = ret[2]
+    print 'Test Set Len: {}'.format(len(testSet))
+    print 'Data sets generated time: {}'.format(datetime.now())
 
     # print 'The balanced data contains {} movies'.format(len(balanced_movies)) 
     
@@ -313,9 +395,21 @@ if __name__ == '__main__':
 
     #returns f(wi, xi)
     probabilityFunctionForDecade = {}
+    decades = [1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010]
     for decade in decades:
         movies_for_decade = filter(lambda x : x["year"] == decade, trainingSet)
         probabilityFunctionForDecade[decade] = generateProbabilityFunctionForData(movies_for_decade)
+    #print probabilityFunctionForDecade
+    print 'Probability functions generated time: {}'.format(datetime.now())
+
+    # probabilityFunctionForDecadeOld = {}
+    # decades = [1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010]
+    # for decade in decades:
+    #     movies_for_decade = filter(lambda x : x["year"] == decade, trainingSet)
+    #     probabilityFunctionForDecadeOld[decade] = generateProbabilityFunctionForDataOld(movies_for_decade)
+
+    # print probabilityFunctionForDecadeOld
+    # print 'Old Probability functions generated time: {}'.format(datetime.now())
 
     bagOfWordsList = []
     for movie in trainingSet:
@@ -323,6 +417,8 @@ if __name__ == '__main__':
 
     #this is the bag of words for the balanced set of movies
     bagOfWords = set(bagOfWordsList)
+
+    print 'Bag of words generated time: {}'.format(datetime.now())
 
     def createFeatureVectorFromWordMap(word_map):
         feature_vector = word_map.copy()
@@ -338,28 +434,36 @@ if __name__ == '__main__':
         #print feature_vector
         return feature_vector
 
+    zeroProbabilitySums = {}
+    for decade in decades:
+
+        zeroProbabilitySum = reduce(lambda sum, word: sum + log(probabilityFunctionForDecade[decade](word, 0)), bagOfWords, 0)
+        zeroProbabilitySums[decade] = zeroProbabilitySum
 
     def computeProbabilityForDecadeAndWordMap(decade, word_map):
 
-        print 'creating feature vector'
-        featureVector = createFeatureVectorFromWordMap(word_map)
+        #print 'creating feature vector'
+        #featureVector = createFeatureVectorFromWordMap(word_map)
+        zeroProbability = zeroProbabilitySums[decade]
         probabilityFunction = probabilityFunctionForDecade[decade]
 
         def probabilityReducer(accumulatedLogProbabilty, feature):
             #print feature
             probability = probabilityFunction(feature[0], feature[1])
+            zeroProbForThisWord = probabilityFunction(feature[0], 0)
             #print probability
-            return accumulatedLogProbabilty + log(probability)
+            return accumulatedLogProbabilty + log(probability) - log(zeroProbForThisWord)
             #return 0
         
-        probability = reduce(probabilityReducer, featureVector.iteritems(), 0)
+        #probability = reduce(probabilityReducer, featureVector.iteritems(), zeroProbability)
+        probability = reduce(probabilityReducer, word_map.iteritems(), zeroProbability)
         #probability = 0
         return probability
 
 
 
-    movie = moviesDict[moviesDict.keys()[0]]
-    decade = 1930
+    # movie = moviesDict[moviesDict.keys()[0]]
+    # decade = 1930
 
     # print '*****************************'
     # print 'Computing probability for {}'.format(movie['title'])
@@ -367,21 +471,67 @@ if __name__ == '__main__':
     # print '{}: {}'.format(decade, probability)
 
 
-    probabilities = {}
+    mainPredictions = np.empty([len(decades)])
     for movie in moviesDict.itervalues():
         print '*****************************'
         print 'Computing probabilities for {}'.format(movie['title'])
 
-        probabilities[movie['title']] = []
+        #probabilities[movie['title']] = []
 
-        for decade in decades:
-            probability = computeProbabilityForDecadeAndWordMap(decade, movie['word_map'])
+        for i, decade in enumerate(decades):
+            mainPredictions[i] = computeProbabilityForDecadeAndWordMap(decade, movie['word_map'])
 
-            print '{}: {}'.format(decade, probability)
-            probabilities[movie['title']].append(probability)
+            #print '{}: {}'.format(decade, probability)
+            #probabilities[movie['title']].append(probability)
 
-    
+        correctDecade = movie['year']
+        predictedDecade = decades[np.argmax(mainPredictions)]
+        # print predictions[i]
+        print 'Correct Decade: {}, Prediced Decade: {}\n'.format(correctDecade, predictedDecade)
 
+    print 'Major Movies time: {}'.format(datetime.now())
+
+
+    #create numpy (n*m, s.t. n=length of training set, m=# of decades) array
+    predictions = np.empty([len(testSet), len(decades)])
+    for i, movie in enumerate(testSet):
+        for j, decade in enumerate(decades):
+            predictions[i, j] = computeProbabilityForDecadeAndWordMap(decade, movie['word_map'])
+
+    #2k - expected accuracy of the classifier
+    correctPredictions = 0
+    for i, movie in enumerate(testSet):
+        correctDecade = movie['year']
+        predictedDecade = decades[np.argmax(predictions[i])]
+        # print predictions[i]
+        # print 'Correct Decade: {}, Prediced Decade: {}'.format(correctDecade, predictedDecade)
+        if correctDecade == predictedDecade:
+            correctPredictions += 1
+
+    print 'Correctly predicted {} out of {}, for an accuracy of {}'.format(correctPredictions, len(testSet), float(correctPredictions)/float(len(testSet)))
+
+    #2l - CMC
+    kData = np.zeros([len(decades)])
+    for i, movie in enumerate(testSet):
+        sortedIndices = list(np.argsort(predictions[i]))
+        sortedIndices.reverse()
+        print sortedIndices
+        correctDecadeIndex = decades.index(movie['year'])
+        print correctDecadeIndex
+        for j in range(len(sortedIndices)):
+            if correctDecadeIndex == sortedIndices[j]:
+                kData[j] += 1
+                break
+
+    #print kData
+    lastSum = 0
+    cmcCurveData = np.empty([len(decades)])
+    for i in range(len(kData)):
+        cmcCurveData[i] = lastSum + kData[i]
+        lastSum = cmcCurveData[i]
+
+
+    print cmcCurveData/len(testSet)
 
 
 
--- plots.py
+++ plots.py
@@ -34,37 +34,37 @@ def plotDecadeHistogram(data, min_decade, max_decade, filename, title, xlabel, y
 	plt.savefig(filename)
 	plt.close()
 
-def plotProbabilityForMovieInDecadeHistogram(data, min_decade, max_decade, filename, title, xlabel, ylabel):
-	# example data
-	# mu = 100 # mean of distribution
-	# sigma = 15 # standard deviation of distribution
-	# x = mu + sigma * np.random.randn(10000)
+# def plotProbabilityForMovieInDecadeHistogram(data, min_decade, max_decade, filename, title, xlabel, ylabel):
+# 	# example data
+# 	# mu = 100 # mean of distribution
+# 	# sigma = 15 # standard deviation of distribution
+# 	# x = mu + sigma * np.random.randn(10000)
 
-	# num_bins = 50
-	# the histogram of the data
+# 	# num_bins = 50
+# 	# the histogram of the data
 
-	num_bins = (max_decade - min_decade) / 10 + 1
-	print num_bins
+# 	num_bins = (max_decade - min_decade) / 10 + 1
+# 	print num_bins
 
-	n, bins, patches = plt.hist(data, num_bins, range=(min_decade,max_decade), , facecolor='green', alpha=0.5)
-	#n, bins, patches = plt.hist(data, num_bins, facecolor='green', alpha=0.5)
+# 	n, bins, patches = plt.hist(data, num_bins, range=(min_decade,max_decade), , facecolor='green', alpha=0.5)
+# 	#n, bins, patches = plt.hist(data, num_bins, facecolor='green', alpha=0.5)
 
-	print n
-	print sum(n)
-	print bins
-	print patches
+# 	print n
+# 	print sum(n)
+# 	print bins
+# 	print patches
 
-	# add a 'best fit' line
-	# y = mlab.normpdf(bins, mu, sigma)
-	# plt.plot(bins, y, 'r--')
-	plt.xlabel(xlabel)
-	plt.ylabel(ylabel)
-	plt.title(title)
+# 	# add a 'best fit' line
+# 	# y = mlab.normpdf(bins, mu, sigma)
+# 	# plt.plot(bins, y, 'r--')
+# 	plt.xlabel(xlabel)
+# 	plt.ylabel(ylabel)
+# 	plt.title(title)
 
-	# Tweak spacing to prevent clipping of ylabel
-	plt.subplots_adjust(left=0.15)
-	plt.savefig(filename)
-	plt.close()
+# 	# Tweak spacing to prevent clipping of ylabel
+# 	plt.subplots_adjust(left=0.15)
+# 	plt.savefig(filename)
+# 	plt.close()
 
 
 if __name__ == '__main__':
